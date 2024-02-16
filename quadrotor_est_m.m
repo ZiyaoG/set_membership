b4197bce10a691f61d5131d77953b99473c97fb4
ZiyaoG <ziyaoguo2020@gmail.com>
2024-02-16 13:35:05 -0600

    updates

--- quadrotor_est_m.m
+++ quadrotor_est_m.m
@@ -1,3 +1,6 @@
+clear all;
+close all;
+load("datas\quadrotors\Trajecory_Task1_constMdist05.mat");
 % % Analysis the datas from geometric controlled quadrototrs and use 
 % % set membership to give estimation of the m and J. 
 % clear all;
@@ -26,17 +29,70 @@
 % 
 % % fname = sprintf('figs/quadrotors/Task%0.3f_Bound%0.3f.png', flag,Bound);
 % % exportgraphics(gcf,fname)
-
-%% Another way to do this
-clear all;
-close all;
-load("datas\quadrotors\Trajecory_Task1_constMdist05.mat");
-
-loose=2;
-B=loose*[0.5;0.5;0.5];
+%% estimate the m_max_bar and m_max_est
+m_est=sdpvar(1,1);
+Constraints= -m_est<=0;
+data_range=10:2:100;
 i=1000;
-data_number=40;
-step=3;
+step=7;
+tmp=[];
+nx=3;
+nz=3;
+O1=3; % parameter in tilde O notation
+% O2=7; % parameter in tilde O notation
+m_max_bar=[];
+m_max_est=[];
+for data_number=data_range
+    for n=1:data_number
+        j=step*n;
+        tmp=[tmp,max(abs(m_est/dt * (v(:,i+j+1) - v(:,i+j)) - m_est*g*e3+f(1,i+j)*R(:,:,i+j)*e3))];
+    end
+    Objective= max(tmp);
+    options = sdpsettings('verbose', 0, 'solver', 'mosek'); % Choose a solver that is available and appropriate
+    sol = optimize(Constraints, Objective, options);
+    if sol.problem == 0
+            m_est_value = double(m_est);
+            % disp(m_est_value)
+            obj_value=double(Objective);
+            m_max_bar=[m_max_bar,obj_value];
+            % disp(obj_value);
+            g_T=nx^1.5*nz^2/data_number;
+            delta_T=max(0,O1*g_T*log(g_T));
+            % disp(delta_T);
+            m_max_est=[m_max_est,obj_value+delta_T];
+            % disp(m_max_est)
+    else
+            disp('No solution found');
+            return;
+    end
+    
+end
+% % check the convergence of delta_T
+% delta_T_series=[];
+% for i=10:100
+%     g_T=nx^1.5*nz^2/i;
+%     delta_T_series=[delta_T_series,O1*g_T*log(g_T)]
+% end
+% plot(delta_T_series)
+%% plot
+plot(data_range,m_max_bar)
+hold on
+plot(data_range,m_max_est)
+legend('$\bar{m}_{max}$','$\hat{m}_{max}$','Interpreter','latex')
+xlabel('number of datapoints')
+ylabel('$m_{max}$','Interpreter','latex')
+% ylim([0,1])
+
+fname = sprintf('figs/quadrotors/convergenceofm_max.png');
+exportgraphics(gcf,fname)
+return;
+%% Another way to do this
+
+loose=2;   % 1 for tight bound, >1 for conservative bound
+B=loose*[0.5;0.5;0.5];
+i=1000;  % first datapoint index to start with
+data_number=20;   % number of datapoints used to generate the sets
+step=7;  % step between two adjacent datapoints 
 Diameter_m=zeros([data_number,1]);
 m_minmax=zeros([data_number,2]);
 m1=sdpvar(1,1);
@@ -85,5 +141,5 @@ xlabel('Datapoints');
 ylabel('Diameter of J: max|m1-m2|')
 title('Estimation Bound of m')
 % 
-fname = sprintf('figs/quadrotors/mbound_Task%0.1f_bound%0.1f_step%0.1f.png', flag.traj,loose,step);
-exportgraphics(gcf,fname)
+% fname = sprintf('figs/quadrotors/mbound_Task%0.1f_bound%0.1f_step%0.1f.png', flag.traj,loose,step);
+% exportgraphics(gcf,fname)
